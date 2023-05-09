68c630974caf3360e524a51bbe370b3d93f1d747
HAN <hhs8028@naver.com>
2023-05-09 17:26:15 +0900

    test1

--- src/App.jsx
+++ src/App.jsx
@@ -11,42 +11,53 @@ import { delCookie } from "./utils/cookie";
 
 function App() {
   const [loading, setLoading] = useState(false);
-  const liffId = '1657832968-ZeGx3gbz'
+  const liffId = "1657832968-ZeGx3gbz";
 
   useEffect(() => {
-    //axios 호출시 인터셉트
-    axios.interceptors.request.use(function (config) {
-      if (config.url !== '/api/search/tags') { // '/api/search/tags' API 요청이 아닌 경우만 로딩 상태 변경
-        setLoading(true);
+    //axios 호출시 인터셉트-----
+    axios.interceptors.request.use(
+      function (config) {
+        if (config.url !== "/api/search/tags") {
+          // '/api/search/tags' API 요청이 아닌 경우만 로딩 상태 변경
+          setLoading(true);
+        }
+        return config;
+      },
+      function (error) {
+        return Promise.reject(error);
       }
-      return config
-    }, function (error) {
-      return Promise.reject(error);
-    });
+    );
     //axios 호출 종료시 인터셉트
-    axios.interceptors.response.use(function (response) {
-      if (response.config.url !== '/api/search/tags') { // '/api/search/tags' API 요청이 아닌 경우만 로딩 상태 변경
+    axios.interceptors.response.use(
+      function (response) {
+        if (response.config.url !== "/api/search/tags") {
+          // '/api/search/tags' API 요청이 아닌 경우만 로딩 상태 변경
+          setLoading(false);
+        }
+        return response;
+      },
+      function (error) {
+        if (error.response.data?.resultCode === "403") {
+          alert("認証がタイムオーバーになりました。再度ログインしてください。");
+          delCookie("accessToken");
+          delCookie("lastLoginTime");
+          window.location.href = "/";
+        }
         setLoading(false);
+        return Promise.reject(error);
       }
-      return response;
-    }, function (error) {
-      if (error.response.data?.resultCode === "403") {
-        alert("認証がタイムオーバーになりました。再度ログインしてください。")
-        delCookie("accessToken");
-        delCookie("lastLoginTime");
-        window.location.href = "/"
-      }
-      setLoading(false)
-      return Promise.reject(error);
-    });
+    );
   }, []);
-
 
   return (
     <RecoilRoot>
       <Loading loading={loading} />
       <BrowserRouter>
-        <GoogleOAuthProvider clientId={'994073566161-uheufnfp50scmu1lquhkg0mdbpr7ip56.apps.googleusercontent.com'}>
+        <GoogleOAuthProvider
+          clientId={
+            "994073566161-uheufnfp50scmu1lquhkg0mdbpr7ip56.apps.googleusercontent.com"
+          }
+        >
           <LiffProvider liffId={liffId}>
             <Router />
           </LiffProvider>
