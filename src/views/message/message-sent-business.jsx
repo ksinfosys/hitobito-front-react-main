f4b76333b73e4c0c4d43ef5f48b06a795a29ab80
sproutish0201 <sproutish0201@gmail.com>
2023-05-10 11:50:20 +0900

    msg

--- src/views/message/message-sent-business.jsx
+++ src/views/message/message-sent-business.jsx
@@ -256,7 +256,7 @@ function MessageSentBusiness() {
                             <div className="flex space-between">
                                 <div className="flex items-center gap-2">
                                     <select
-                                        className="form-select w-32"
+                                        className="form-select w-32 lg:w-36 shrink-0"
                                         onChange={(e) => setSearchCategory(e.target.value)}
                                     >
                                         <option value="20601">受信者</option>
